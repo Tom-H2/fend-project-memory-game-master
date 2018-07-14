7d2d31c10482d5bfad3334c71a880bc3f62c485f
Thomas Harris <lostmt.tom@gmail.com>
2018-07-14 09:35:08 -0700

    style Runs code through formatter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 let deck = document.querySelector('.deck'); //This function shuffles the deck every time the page is opened or reset
-let turnCards = document.querySelectorAll('.card');//sets variable for individual card
+let turnCards = document.querySelectorAll('.card'); //sets variable for individual card
 let stars = document.querySelectorAll('.fa-star');
 let clock = document.querySelector('.clock');
 let numMoves = document.querySelector('.moves');
@@ -25,25 +25,26 @@ let starCount;
 
 
 function newDeck() { //function that shuffles the deck
-    const shuffleDeck = Array.from(document.querySelectorAll('.card'));
-    const newShuffle = shuffle(shuffleDeck);
-    for (card of newShuffle) {
-      deck.appendChild(card);
-    }
+	const shuffleDeck = Array.from(document.querySelectorAll('.card'));
+	const newShuffle = shuffle(shuffleDeck);
+	for (card of newShuffle) {
+		deck.appendChild(card);
+	}
 }
 newDeck();
 
 function shuffle(array) { //This code was provided in initial set up code
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length,
+		temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
 
@@ -58,167 +59,151 @@ function shuffle(array) { //This code was provided in initial set up code
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-deck.addEventListener('click', function(event) { //adds addEventListener to .deck
-  let turn = event.target;
-  incrementMoves(); //changes the move score
-  keepScore();
+deck.addEventListener('click', function (event) { //adds addEventListener to .deck
+	let turn = event.target;
+	incrementMoves(); //changes the move score
+	keepScore();
 
-  if (turn.classList.contains('card') && openCards.length < 2 && !turn.classList.contains('open', 'show', 'match')) {
-    openCards.push(turn);
-    turn.classList.add('open', 'show');
-  }
+	if (turn.classList.contains('card') && openCards.length < 2 && !turn.classList.contains('open', 'show', 'match')) {
+		openCards.push(turn);
+		turn.classList.add('open', 'show');
+	}
 
-if(openCards.length === 2) {
-  firstCard = openCards[0];
-  secondCard = openCards[1];
+	if (openCards.length === 2) {
+		firstCard = openCards[0];
+		secondCard = openCards[1];
 
-  if (firstCard.firstElementChild.className ===
-    secondCard.firstElementChild.className) {
-      openCards.forEach(function(card) {
-      card.classList.add('match'); //adds 'match' to class 'card' that allows the cards to stay open
-    });
-    gameOver();
-    openCards = [];
-}
-  else  {
-    setTimeout(function(turn) {
-      openCards.forEach(function(turn) {
-      turn.classList.remove('open', 'show');
-    });
-      openCards = [];
-      }, 1000);
-   }
- }
+		if (firstCard.firstElementChild.className ===
+			secondCard.firstElementChild.className) {
+			openCards.forEach(function (card) {
+				card.classList.add('match'); //adds 'match' to class 'card' that allows the cards to stay open
+			});
+			gameOver();
+			openCards = [];
+		} else {
+			setTimeout(function (turn) {
+				openCards.forEach(function (turn) {
+					turn.classList.remove('open', 'show');
+				});
+				openCards = [];
+			}, 1000);
+		}
+	}
 });
 
 let mouseClicks = 0;
 
 function incrementMoves() { //listens to mouse clicks and increments up the Moves text. This function is called on line 66
-  mouseClicks++;
-  numMoves.innerHTML = mouseClicks;
-  if (mouseClicks === 1) {
-    startTime();
-  }
+	mouseClicks++;
+	numMoves.innerHTML = mouseClicks;
+	if (mouseClicks === 1) {
+		startTime();
+	}
 };
 
 function keepScore() { //tracks the number of mouse clicks from incrementMoves function the console.log tells what I want to happen to stars
-  let thirdStar = document.getElementById('thirdStar'),
-      secondStar = document.getElementById('secondStar'),
-      firstStar = document.getElementById('firstStar');
-
-  if (mouseClicks === 20) {
-    thirdStar.classList.add('fa-star-o');
-    starCount = 2;
-  }
-  else if (mouseClicks === 34) {
-    secondStar.classList.add('fa-star-o');
-    starCount = 1;
-  }
-  else if (mouseClicks === 40) {
-    thirdStar.classList.add('fa-star-o');
-    starCount = 0;
-  }
+	let thirdStar = document.getElementById('thirdStar'),
+		secondStar = document.getElementById('secondStar'),
+		firstStar = document.getElementById('firstStar');
+	if (mouseClicks < 20) {
+		starCount = 3;
+	} else if (mouseClicks === 20) {
+		thirdStar.classList.add('fa-star-o');
+		starCount = 2;
+	} else if (mouseClicks === 34) {
+		secondStar.classList.add('fa-star-o');
+		starCount = 1;
+	} else if (mouseClicks === 40) {
+		thirdStar.classList.add('fa-star-o');
+		starCount = 0;
+	}
 }
 
 function startTime() {
-  time = 0;
-  clockId = setInterval(() => {
-    time++;
-    clock.innerHTML = time;
-  }, 1000);
+	time = 0;
+	clockId = setInterval(() => {
+		time++;
+		clock.innerHTML = time;
+	}, 1000);
 
 }
 
 let reset = document.querySelector('.restart'); //Event listener for restart button
-  reset.addEventListener('click', function(event) {
-    gameReset();
-  })
+reset.addEventListener('click', function (event) {
+	gameReset();
+})
 
-function callModal () {
-  const modal = document.querySelector('.modal__body');
-  modal.classList.toggle('hide');
+function callModal() {
+	const modal = document.querySelector('.modal__body');
+	modal.classList.toggle('hide');
 }
-  //callModal(); //keeps modal open for testing and styling
+//callModal(); //keeps modal open for testing and styling
 
-function gameOver () {
-  if (firstCard.classList.contains('match') && secondCard.classList.contains('match')) {
-      matchedCards.push(firstCard);
-      matchedCards.push(secondCard);
-      if (matchedCards.length === 16) {
-        callModal();
-        stopTime();
-        finalScores();
-    }
-  }
+function gameOver() {
+	if (firstCard.classList.contains('match') && secondCard.classList.contains('match')) {
+		matchedCards.push(firstCard);
+		matchedCards.push(secondCard);
+		if (matchedCards.length === 16) {
+			callModal();
+			stopTime();
+			finalScores();
+		}
+	}
 }
 
 document.querySelector('.modal__close').addEventListener('click', () => {
-  callModal();
+	callModal();
 });
 
-/*function starCounter () { //This function counts the number of stars
-  if (thirdStar.classList.contains('fa-star')) {
-    starCount = '3';
-  }
-  else if (thirdStar.classList.contains('fa-star-o') && secondStar.classList.contains('fa-star')) {
-    starCount = '2';
-  }
-  else if (thirdStar.classList.contains('fa-star-o') && secondStar.classList.contains('fa-star-o') && firstStar.classList.contains('fa-star')) {
-    starCount = '1';
-  }
-}*/
-
-
-function finalScores () {
-  finalClicks = document.querySelector('.modal__moves');
-    finalClicks.innerHTML = 'Moves = ' + mouseClicks;
-  finalTime = document.querySelector('.modal__time');
-    finalTime.innerHTML = 'Seconds = ' + time;
-  finalStars = document.querySelector('.modal__stars');
-    finalStars.innerHTML = 'Stars = ' + starCount;
-
+function finalScores() {
+	finalClicks = document.querySelector('.modal__moves');
+	finalClicks.innerHTML = 'Moves = ' + mouseClicks;
+	finalTime = document.querySelector('.modal__time');
+	finalTime.innerHTML = 'Seconds = ' + time;
+	finalStars = document.querySelector('.modal__stars');
+	finalStars.innerHTML = 'Stars = ' + starCount;
 }
 
 document.querySelector('.modal__button').addEventListener('click', () => { //replay button on modal
-  mouseClicks = 0;
-  resetStars();
-  resetMoves();
-  resetTime();
-  newDeck();
-  cardReset();
+	mouseClicks = 0;
+	resetStars();
+	resetMoves();
+	resetTime();
+	newDeck();
+	cardReset();
 });
 
-function resetStars () {
-  thirdStar.classList.remove('fa-star-o');
-  secondStar.classList.remove('fa-star-o');
-  firstStar.classList.remove('fa-star-o');
+function resetStars() {
+	thirdStar.classList.remove('fa-star-o');
+	secondStar.classList.remove('fa-star-o');
+	firstStar.classList.remove('fa-star-o');
 }
 
-function resetMoves () {
-  numMoves.innerHTML = 0;
+function resetMoves() {
+	numMoves.innerHTML = 0;
 }
 
-function resetTime () {
-  clock.innerHTML = 0;
+function resetTime() {
+	clock.innerHTML = 0;
 }
 
-function stopTime () {
-  clearInterval(clockId);
+function stopTime() {
+	clearInterval(clockId);
 }
 
-function cardReset () {
-  const cards = document.querySelectorAll('.deck li');
-  for (let card of cards) {
-    card.className = 'card';
-  }
+function cardReset() {
+	const cards = document.querySelectorAll('.deck li');
+	for (let card of cards) {
+		card.className = 'card';
+	}
 }
 
 function gameReset() {
-  mouseClicks = 0;
-  resetStars();
-  resetMoves();
-  resetTime();
-  stopTime();
-  newDeck();
-  cardReset();
+	mouseClicks = 0;
+	resetStars();
+	resetMoves();
+	resetTime();
+	stopTime();
+	newDeck();
+	cardReset();
 }
